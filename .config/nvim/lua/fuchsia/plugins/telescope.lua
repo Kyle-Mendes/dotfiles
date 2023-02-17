a003eb5b922c7d3112298d2d6331694bf4958eaf
Kyle Mendes <kyle.mendes@discordapp.com>
2023-02-17 18:34:12 +0000

    Adding git files commands

--- .config/nvim/lua/fuchsia/plugins/telescope.lua
+++ .config/nvim/lua/fuchsia/plugins/telescope.lua
@@ -30,13 +30,6 @@ function M.config()
             },
         },
     })
-
-	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-	vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 end
 
 return M
